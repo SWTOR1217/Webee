828ec7c1372d445e568f1c1dc4c23e05e62ccc7d
SWTOR1217 <swtor1217@Yutos-MacBook-Pro.local>
2017-08-07 09:43:31 +0900

    First Commit
    First commit of Webee in the Makeschool Summer Academy 2017.

--- Webee/ViewController.swift
+++ Webee/ViewController.swift
@@ -8,18 +8,95 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchControllerDelegate, UISearchResultsUpdating, UISearchBarDelegate, UIWebViewDelegate {
+    
+    
+    @IBOutlet weak var WebView: UIWebView!
+    @IBAction func back(_ sender: Any)
+    {
+        if WebView.canGoBack
+        {
+        WebView.goBack()
+        }
+    }
+    
+    @IBAction func forward(_ sender: Any)
+    {
+    if WebView.canGoForward
+    {
+        WebView.goForward()
+        }
+    }
+    
+    @IBAction func refresh(_ sender: Any)
+    {
+        WebView.reload()
+    }
+    var searchController : UISearchController!
+    
+    func webViewDidStartLoad(_ webView: UIWebView)
+    {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView)
+    {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+        {
+            searchBar.resignFirstResponder()
+            
+            if let url = URL(string: searchBar.text!)
+            {
+                WebView.loadRequest(URLRequest(url: url))
+            }
+            
+            else
+            {
+                print ("There has been an error on your URL load request")
+            }
+            
+        }
+        
+        WebView.loadRequest(URLRequest(url: URL(string: "https://www.google.co.uk/")!))
+        
+        self.searchController = UISearchController(searchResultsController:  nil)
+        
+        self.searchController.searchResultsUpdater = self
+        self.searchController.delegate = self
+        self.searchController.searchBar.delegate = self
+        
+        self.searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.dimsBackgroundDuringPresentation = true
+        
+        self.navigationItem.titleView = searchController.searchBar
+        
+        self.definesPresentationContext = true
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
     }
-
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+        
+    
+       
+    func updateSearchResults(for searchController: UISearchController) {
+      
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+        
 }
-
+}
